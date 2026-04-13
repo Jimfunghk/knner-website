00acc018b9917e9e521b0d2b3ee5af78e84b1424
jfcity <jimlsfung@hotmail.com>
2026-04-13 22:55:07 +0800

    Trigger redeploy

--- src/sections/Download.tsx
+++ src/sections/Download.tsx
@@ -1,3 +1,4 @@
+// trigger redeploy
 import { useEffect, useRef } from 'react';
 import { TracingPaperCard } from '@/components/TracingPaperCard';
 import { useTheme } from '@/contexts/ThemeContext';
